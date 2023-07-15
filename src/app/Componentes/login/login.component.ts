56187374c3984c5c5fa2e6841dd631a3beee6b5b
Anthony_Dev <morante232302@gmail.com>
2023-07-14 19:33:50 -0500

    --configuracion token

--- src/app/Componentes/login/login.component.ts
+++ src/app/Componentes/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastComponent } from 'src/app/Compartidos/Componentes/toast';
+import { LoginService } from 'src/app/Servicios/login.service';
+import jwt_decode from "jwt-decode";
 
 @Component({
   selector: 'app-login',
@@ -6,6 +11,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+
+
+  btnLogin : boolean = false;
+
+  loginForm = new  FormGroup({
+
+    usuario: new FormControl('', Validators.required),
+    clave: new FormControl('', Validators.required)
+
+  });
+
+  ingresarTexto :string="Ingresar";
+
+  token:any;
 
   ngOnInit() {
 
@@ -26,6 +46,47 @@ export class LoginComponent {
   
   
     }
+
+
+    constructor(private toast: ToastComponent ,private loginService: LoginService, private router: Router, private el: ElementRef) {
+    }
+  
+    login(loginData:any){ 
+      
+      this.loginService.login(loginData).subscribe({
+        next: (res) =>{ 
+          
+          if(res){
+            
+            console.log(res);
+            localStorage.setItem("token",res.access_token);
+            this.token = jwt_decode(res.access_token);
+            localStorage.setItem("user",this.token.name);
+            this.router.navigate(['/Inicio'])
+
+          }
+
+        
+        } ,
+        error:(err)=> {
+
+          console.log(err);
+          if(err.error.error_description ==="Incorrect password"){
+ 
+           this.toast.show_error("Login","Incorrect Credentials");
+           return;
+
+          }
+
+          this.toast.show_error("Error","Error Try Later");
+          
+        },
+        })
+
+    }
+  
+  
+
 
   
 
