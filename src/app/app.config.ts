f51002622d9c5915737b7062f5fb3fcbfdea3a94
Juan Carlos Lozada Castillo <jc.lozadacastillo@gmail.com>
2023-11-27 11:31:21 -0500

    cambio

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,8 @@ import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { jsonConfig } from './config';
 
-export const js=window as any;
+export const js = window as any;
 export const global = Object.freeze(jsonConfig);
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes,withHashLocation())]
+  providers: [provideRouter(routes, withHashLocation())]
 };
