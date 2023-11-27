7aa015d78bbbdd26a2eb28956c44ce7fc3a42505
Juan Carlos Lozada Castillo <jc.lozadacastillo@gmail.com>
2023-11-27 11:31:33 -0500

    cambio

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,5 @@ import { jsonConfig } from './config';
 export const js = window as any;
 export const global = Object.freeze(jsonConfig);
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withHashLocation())]
+  providers: [provideRouter(routes)]
 };
