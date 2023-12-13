0192751d57a0ed418d7315c56f2c412e0778aa3e
Juan Carlos Lozada Castillo <jc.lozadacastillo@outlook.com>
2023-12-12 20:01:35 -0500

    terminado configuraciones

--- src/app/Components/configuraciones/configuraciones.component.ts
+++ src/app/Components/configuraciones/configuraciones.component.ts
@@ -15,16 +15,19 @@ export class ConfiguracionesComponent implements OnInit {
   private baseUrl: string = `${global.BASE_API_URL}api/Configuraciones/`;
   public urlLogo: string = "";
   public urlFirma: string = "";
+  private codigo: string = "";
+  private download: boolean = false;
   public datos: any = {};
-  public modal:any;
+  public modal: any;
   public async ngOnInit(): Promise<void> {
     js.loaderShow();
-    this.modal=new js.bootstrap.Modal(js.modalDatos,{
-      keyboard:false,
-      backdrop:"static"
+    this.modal = new js.bootstrap.Modal(js.modalDatos, {
+      keyboard: false,
+      backdrop: "static"
     });
     js.activarValidadores(js.frmDatos);
-    console.log();
+    js.activarValidadores(js.frmFirma);
+    js.crearPasswordPreview(js.frmFirma);
     await this.datosEmpresa();
     js.loaderHide();
   }
@@ -72,42 +75,77 @@ export class ConfiguracionesComponent implements OnInit {
     }
   }
   public async handleFirma(): Promise<void> {
-    try{
-    this.urlFirma = `${global.BASE_API_URL}${this.datos.ruta}`.replaceAll("//", "/");
-    var archivoExistente = await new Promise(async resolve => {
-      try {
-        const url = this.urlFirma;
-        await js.fetch(url, {
-          method: "HEAD",
-          mode: 'no-cors'
-        });
-        this.urlFirma = url;
-        resolve(true);
-      } catch {
-        this.urlFirma = "";
-        resolve(false);
+    try {
+      this.download = false;
+      this.urlFirma = `${global.BASE_API_URL}${this.datos.ruta}`.replaceAll("//", "/");
+      var archivoExistente = await new Promise(async resolve => {
+        try {
+          const url = this.urlFirma;
+          await js.fetch(url, {
+            method: "HEAD",
+            mode: 'no-cors'
+          });
+          this.urlFirma = url;
+          resolve(true);
+        } catch {
+          this.urlFirma = "";
+          resolve(false);
+        }
+      });
+      (!archivoExistente) ?
+        js.archivoFirma.removeAttribute("data-validate") :
+        js.archivoFirma.setAttribute("data-validate", "no-validate");
+      js.activarValidadores(js.archivoFirma.closest("div"));
+      if (!!js.archivoFirma.value) {
+        const file = js.archivoFirma.files[0];
+        if (file.type.toString().indexOf("x-pkcs12") < 0) throw new Error("El archivo de imagen debe ser de tipo P12");
+        this.modal.show();
       }
-    });
-    (!archivoExistente) ?
-      js.archivoFirma.removeAttribute("data-validate") :
-      js.archivoFirma.setAttribute("data-validate", "no-validate");
-    js.activarValidadores(js.archivoFirma.closest("div"));
-    if (!!js.archivoFirma.value) {
-      const file = js.archivoFirma.files[0];
-      if (file.type.toString().indexOf("x-pkcs12") < 0) throw new Error("El archivo de imagen debe ser de tipo P12")
-    }
-  }catch(e){
+    } catch (e) {
       js.handleError(e);
       js.archivoFirma.value = "";
-  }
+    }
   }
 
   public descargarFirma() {
+    js.limpiarForm(js.frmFirma);
+    this.download = true;
     this.modal.show();
   }
 
-  public async validarFirma():Promise<void>{
+  public async validarFirma(): Promise<boolean> {
+      return new Promise(async resolve=>{
+        try {
+          const url = `${this.baseUrl}validarFirma`;
+          const data = new FormData(js.frmFirma);
+          let res = (await this._axios.postForm(url, data)).data;
+          res = res.toString().split("error:");
+          if (res.length > 1) throw new Error(res[1]);
+          if (this.download) js.open(this.urlFirma, "_blank");
+          this.codigo = js.codigo.value;
+          js.toastSuccess(res[0]);
+          js.limpiarForm(js.frmFirma);
+          this.modal.hide();
+          resolve(true);
+        } catch (e) {
+          js.handleError(e);
+          resolve(false);
+        }
+      });
+  }
 
+  public async guardar(){
+    try {
+      if(!await js.validarTodo(js.frmDatos)) throw new Error("Verifique los campos requeridos");
+      const url=`${this.baseUrl}guardar`;
+      const data=new FormData(js.frmDatos);
+      data.append("codigo",this.codigo);
+      await this._axios.postForm(url,data);
+      js.toastSuccess("Configuración guardada con éxito");
+      this.datosEmpresa();
+    } catch (e) {
+      js.handleError(e);
+    } 
   }
 
 }
